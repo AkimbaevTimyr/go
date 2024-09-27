ec4dc35547f8ee1c6bfa904ed80a38cb0f995bca
timur <akimbaev.tim2002@gmail.com>
2024-09-27 20:01:40 +0500

    add env & load env variables

--- database/db.go
+++ database/db.go
@@ -2,16 +2,38 @@ package database
 
 import (
 	"akimbaev/models"
+	"fmt"
 	"log"
+	"os"
+	"regexp"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+const projectDirName = "go"
+
 func Init() {
-	dsn := "host=localhost user=postgres password=newpassword dbname=testgo port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+
+	e := godotenv.Load(string(rootPath) + `/.env`)
+	if e != nil {
+		log.Fatal(e.Error())
+	}
+
+	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	database := os.Getenv("DATABASE")
+	port := os.Getenv("PORT")
+	sllmode := os.Getenv("SLLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai", host, user, password, database, port, sllmode)
 	var err error
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
