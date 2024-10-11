f16574ae23066d4b78713a7a13231a3b23cb8999
timur <akimbaev.tim2002@gmail.com>
2024-10-11 16:36:55 +0500

    delete order rep, add validation on login request struct

--- requests/LoginRequest.go
+++ requests/LoginRequest.go
@@ -1,8 +1,8 @@
 package requests
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,min=10,max=50"`
+	Password string `json:"password" validate:"required,min=8,max=20"`
 }
 
 type CheckCodeRequest struct {
